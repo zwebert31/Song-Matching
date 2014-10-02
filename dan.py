2dfa640af9e2e307ab137c4123a1ef8ee3fd85ef
Zachary J. Webert <zwebert@hook.ccs.neu.edu>
2014-10-02 19:35:39 -0400

    no such file error catching

--- dan.py
+++ dan.py
@@ -4,12 +4,18 @@ import sys
 import wave
 import struct
 import numpy
+import os.path
 def songLength(waveFile):
     return waveFile.getnframes()/float(waveFile.getframerate())
 
 def isMatch(wavePath1, wavePath2):
+    if not os.path.isfile(wavePath1) or not os.path.isfile(wavePath2):
+        sys.stderr.write("ERROR\n")
+        exit(1)
+
     wave1 = wave.open(wavePath1, 'r')
     wave2 = wave.open(wavePath2, 'r')
+
     if songLength(wave1) != songLength(wave2):
         return "NO MATCH"
     else:
