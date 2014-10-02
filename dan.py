bdc406c69335af472d94df7651ba6bce906c508c
Zachary J. Webert <zwebert@hook.ccs.neu.edu>
2014-10-02 19:26:23 -0400

    length matching

---
+++ dan.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+
+import sys
+import wave
+import struct
+import numpy
+def songLength(waveFile):
+    return waveFile.getnframes()/float(waveFile.getframerate())
+
+def isMatch(wavePath1, wavePath2):
+    wave1 = wave.open(wavePath1, 'r')
+    wave2 = wave.open(wavePath2, 'r')
+    if songLength(wave1) != songLength(wave2):
+        return "NO MATCH"
+    else:
+        return "MATCH"
+
+print isMatch(sys.argv[2], sys.argv[4])
+
+#if sys.argv[1] != '-f' or sys.argv[3] != '-f':
+#    print("Error incorrect syntax")
+#    exit(1);
+#else:	
+#    waveFile = wave.open(sys.argv[2], 'r')
+    
+#    nchannels, sampwidth, framerate, nframes, comptype, compname =  waveFile.getparams()
+#    print struct.unpack("HH", waveFile.readframes(1))
+#    print songLength(nframes, framerate)
+#for i in range(0,length):
+
+        
--- matcher.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-
-import sys
-import wave
-if sys.argv[1] != '-f' or sys.argv[3] != '-f':
-    print("Error incorrect syntax");
-    exit(1);
-else:	
-    print(sys.argv[2]);
